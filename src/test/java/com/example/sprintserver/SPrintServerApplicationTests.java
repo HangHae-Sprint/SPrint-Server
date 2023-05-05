786bd5d3ced6f7d0ed4637d6a09bafa9d6ae04ff
mottoslo <c4save@korea.ac.kr>
2023-05-05 20:56:55 +0900

    backend project init

---
+++ src/test/java/com/example/sprintserver/SPrintServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SPrintServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/example/sprintserver/SpringSecurity/dd.java
@@ -0,0 +1,5 @@
+package com.example.sprintserver.SpringSecurity;
+
+public class dd {
+}
+
