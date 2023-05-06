f28d9fa39680d0dcbf6240d737ef9ed28fe59a90
mottoslo <c4save@korea.ac.kr>
2023-05-06 11:37:12 +0900

    feat : common->Message

---
+++ src/main/java/com/example/sprintserver/sprint/controller/SprintController.java
@@ -0,0 +1,15 @@
+package com.example.sprintserver.sprint.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/sprint")
+public class SprintController {
+
+
+}
