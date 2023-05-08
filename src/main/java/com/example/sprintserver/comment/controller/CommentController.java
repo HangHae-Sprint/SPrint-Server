dc33899f08ec1472117f2f54a5f2cd6eeb7c82ee
kmg0485 <kmg0485@naver.com>
2023-05-08 19:59:06 +0900

    Feat: comment 관련 기능들 수정

---
+++ src/main/java/com/example/sprintserver/comment/controller/CommentController.java
@@ -0,0 +1,46 @@
+package com.example.sprintserver.comment.controller;
+
+import com.example.sprintserver.comment.dto.CommentRequestDto;
+import com.example.sprintserver.comment.service.CommentService;
+import com.example.sprintserver.common.Message;
+import com.example.sprintserver.common.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/comment")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    // 댓글 작성
+    @PostMapping("/{id}")
+    public ResponseEntity<Message> addComment(
+            @PathVariable Long id,
+            @RequestBody CommentRequestDto commentRequestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(commentService.addComment(id, commentRequestDto, userDetails.getUser()));
+    }
+
+    // 댓글 수정
+    @PutMapping("/{sprintId}/{commentId}")
+    public ResponseEntity<Message> updateComment(
+            @PathVariable Long sprintId,
+            @PathVariable Long commentId,
+            @RequestBody CommentRequestDto commentRequestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(commentService.updateComment(sprintId, commentId, commentRequestDto, userDetails.getUser()));
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{sprintId}/{commentId}")
+    public ResponseEntity<Message> deleteComment(
+            @PathVariable Long sprintId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(commentService.deleteComment(sprintId, commentId, userDetails.getUser()));
+    }
+}
