9c8fb43663d01b276db48220f9ce00cb5b94dc83
mottoslo <c4save@korea.ac.kr>
2023-05-08 17:18:44 +0900

    disable comment package for testing

--- src/main/java/com/example/sprintserver/comment/repository/CommentRepository.java
+++ src/main/java/com/example/sprintserver/comment/repository/CommentRepository.java
@@ -1,13 +1,13 @@
-package com.example.sprintserver.comment.repository;
-
-import com.example.sprintserver.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-import java.util.Optional;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllBySprint_IdOrderByCreatedAt(Long SPRINT_ID);
-    void deleteAllBySprint_Id(Long SPRINT_ID);
-    Optional<Comment> findByIdAndUserId(Long CommentId, Long UserId);
-}
+//package com.example.sprintserver.comment.repository;
+//
+//import com.example.sprintserver.comment.entity.Comment;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.util.List;
+//import java.util.Optional;
+//
+//public interface CommentRepository extends JpaRepository<Comment, Long> {
+//    List<Comment> findAllBySprint_IdOrderByCreatedAt(Long SPRINT_ID);
+//    void deleteAllBySprint_Id(Long SPRINT_ID);
+//    Optional<Comment> findByIdAndUserId(Long CommentId, Long UserId);
+//}
