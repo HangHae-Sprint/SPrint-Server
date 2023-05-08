52070c5c889eb21f5e5f6e9392ca210a62421396
mottoslo <c4save@korea.ac.kr>
2023-05-08 19:15:11 +0900

    swagger fox 주석처리 (jacksonlibrary충돌)

--- src/main/java/com/example/sprintserver/common/swagger/SwaggerConfig.java
+++ src/main/java/com/example/sprintserver/common/swagger/SwaggerConfig.java
@@ -1,37 +1,37 @@
-package com.example.sprintserver.common.swagger;
-
-import com.fasterxml.classmate.TypeResolver;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-@EnableWebMvc
-@Configuration
-public class SwaggerConfig extends WebMvcConfigurationSupport {
-
-    @Bean
-    public Docket api(TypeResolver typeResolver) {
-        return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(true)
-                .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.sprintserver"))
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("SPRINT Api Document")
-                .description("Api document for SPRINT PROJECT")
-                .version("1.0")
-                .build();
-    }
-}
+//package com.example.sprintserver.common.swagger;
+//
+//import com.fasterxml.classmate.TypeResolver;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//
+//@EnableWebMvc
+//@Configuration
+//public class SwaggerConfig extends WebMvcConfigurationSupport {
+//
+//    @Bean
+//    public Docket api(TypeResolver typeResolver) {
+//        return new Docket(DocumentationType.OAS_30)
+//                .useDefaultResponseMessages(true)
+//                .apiInfo(apiInfo())
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.example.sprintserver"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    private ApiInfo apiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("SPRINT Api Document")
+//                .description("Api document for SPRINT PROJECT")
+//                .version("1.0")
+//                .build();
+//    }
+//}
--- src/main/java/com/example/sprintserver/sprint/dto/FieldTupleDto.java
+++ src/main/java/com/example/sprintserver/sprint/dto/FieldTupleDto.java
@@ -1,16 +1,13 @@
 package com.example.sprintserver.sprint.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
+@NoArgsConstructor
 public class FieldTupleDto {
-    private final String fieldName;
-    private final Integer fieldMaxNum;
-
-    public FieldTupleDto(String fieldName, Integer fieldMaxNum) {
-        this.fieldName = fieldName;
-        this.fieldMaxNum = fieldMaxNum;
-    }
+    private String fieldName;
+    private Integer fieldMaxNum;
 }
