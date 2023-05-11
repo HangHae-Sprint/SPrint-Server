7988e26ff735dd233001e560349c49c8eeb150be
InyongHwang <hiy1934@gmail.com>
2023-05-11 15:00:27 +0900

    Fix : add User domain exception handling, SignupRequestDto field pattern validate

--- src/main/java/com/example/sprintserver/user/controller/UserController.java
+++ src/main/java/com/example/sprintserver/user/controller/UserController.java
@@ -5,13 +5,16 @@ import com.example.sprintserver.user.dto.LoginRequestDto;
 import com.example.sprintserver.user.dto.SignupRequestDto;
 import com.example.sprintserver.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@Controller
+@RestController
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
@@ -19,7 +22,15 @@ public class UserController {
     private final UserService userService;
     @ResponseBody
     @PostMapping("/register")
-    public Message signup(@Valid @RequestBody SignupRequestDto signupRequestDto) {
+    public Message signup(@Valid @RequestBody SignupRequestDto signupRequestDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            StringBuilder sb = new StringBuilder();
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                sb.append(fieldError.getDefaultMessage());
+            }
+            System.out.println(sb);
+            return new Message(sb.toString(), HttpStatus.BAD_REQUEST);
+        }
         return userService.signup(signupRequestDto);
     }
 
@@ -29,4 +40,8 @@ public class UserController {
         return userService.login(loginRequestDto, response);
     }
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Message userExceptionHandler(Exception e) {
+        return new Message(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/example/sprintserver/user/dto/SignupRequestDto.java
+++ src/main/java/com/example/sprintserver/user/dto/SignupRequestDto.java
@@ -10,18 +10,18 @@ import javax.validation.constraints.Pattern;
 @Setter
 public class SignupRequestDto {
 
-//    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z]).{4,10}$",
-//            message = "아이디는 4-10자 알파벳 소문자, 숫자로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z]).{2,15}$",
+            message = "아이디는 2-15자 알파벳 소문자, 숫자로 입력해주세요.")
     private String username;
 
-//    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])(?=.*[~!@#$%^&*()_+|<>?:{}])(?=\\S+$).{8,15}$",
-//            message = "비밀번호는 8-15자 알파벳 대소문자, 숫자, 특수문자로 작성해주세요.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-z]).{2,15}$",
+            message = "비밀번호는 2-15자 알파벳 소문자, 숫자로 입력해주세요.")
     private String password;
 
-//    @NotNull(message = "닉네임을 입력해주세요.")
+    @NotNull(message = "닉네임을 한글자 이상 입력해주세요.")
     private String nickname;
 
-//    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
-//            message = "이메일 형식이 올바지르 않습니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$",
+            message = "이메일 형식이 올바르지 않습니다.")
     private String email;
 }
